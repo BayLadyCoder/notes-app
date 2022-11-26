7cf51ca2308330b48f75eb056d70cbdd582c3858
BayLadyCoder <rachada_32@hotmail.com>
2022-11-26 14:41:25 -0500

    Create AddTag() and availableTags

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { NoteForm } from './NoteForm';
-import { NoteData } from './types';
+import { NoteData, Tag } from './types';
 
 type NewNoteProps = {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-export const NewNote = ({ onSubmit }: NewNoteProps) => {
+export const NewNote = ({
+  onSubmit,
+  onAddTag,
+  availableTags,
+}: NewNoteProps) => {
   return (
     <>
       <h1 className='mb-4'>NewNote</h1>
-      <NoteForm onSubmit={onSubmit} />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 };
